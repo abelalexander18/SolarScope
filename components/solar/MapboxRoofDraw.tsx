82220a221bfd7450273e6775b786af78fbf321fb
Abel Alexander <abelalexander84@gmail.com>
2026-09-17 14:28:13 +0530

    Fix proxy export and install missing dependencies

--- components/solar/MapboxRoofDraw.tsx
+++ components/solar/MapboxRoofDraw.tsx
@@ -3,7 +3,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import MapboxDraw from "@mapbox/mapbox-gl-draw";
-import { area } from "@turf/area";
+import area from "@turf/area";
 
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
